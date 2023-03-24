93d6a6948e85dfe2fc2c8a2d94f2595d47e68f22
KamillySantana <kamillyferreira0300@gmail.com>
2023-03-24 08:06:01 -0300

    html

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel admistrativo</title>
+</head>
+<body>
+    <div class="div-global">
+
+        <div class="div-menu-lateral">
+            <img src="img/img/senai-logo1.png" alt="">
+
+            <button>Painel de controle</button>
+            <button>Usuários</button>
+            <button>Minha conta</button>
+            <button>Segurança</button>
+            <button>Corporativo</button>
+            <button>Esportes</button>
+            <button>Acadêmico</button>
+            <button>Relatórios</button>
+            <button>Custumozação</button>
+            <button>Alertas</button>
+        </div>
+
+        <div class="div-conteudo">
+
+            <header class="topo">
+                <nav>
+                    <a href="">Dashboards</a>
+                    <a href="">Páginas</a>
+                    <a href="">Aplicativos</a>
+                    <a href="">Ajuda</a>
+                </nav>
+
+                <form action="" method="POST">
+                    <input type="text">
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </header>
+
+            <main class="corpo">
+
+                <div class="painel-controle">
+                    <h1>E-commerce | Painel de Controle</h1>
+
+                    <div class="vendas-mes">
+                        <p>aqui vai ser o valor</p>
+                        <p>Vendas esse mês</p>
+                        <i class="fa-solid fa-coins"></i>
+                    </div>
+
+                    <div class="produtos-ativos">
+                        <p>345</p>
+                        <p>Produtos ativos</p>
+                    </div>
+
+                    <div class="fornecedores">
+                        <p>25</p>
+                        <p>Fornecedores</p>
+                    </div>
+
+                    <div class="clientes">
+                        <p>4500</p>
+                        <p>Clientes</p>
+                    </div>
+
+                </div>
+
+                <div class="carrinho">
+                    <div class="carrinho-ativo">
+
+                    </div>
+
+                    <div class="novo-aplicativo">
+
+                    </div>
+                </div>
+                
+                <div class="pedido-vendas">
+                    <div class="pedido">
+
+                    </div>
+
+                    <div class="venda">
+
+                    </div>
+                </div>
+
+            </main>
+
+            <footer class="rodape">
+                <p>2023© Senai Livre - Sistema de Controle</p>
+            </footer>
+        </div>
+
+    </div>
+        <script src="https://kit.fontawesome.com/45751c5e31.js" crossorigin="anonymous"></script>
+</body>
+</html>
