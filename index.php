b533a033fc17fc062437b26e105b68e3fd0d2c66
basqueoro <basqueroto.felipe125@gmail.com>
2023-03-24 17:14:23 -0300

    término das funcionalidade

--- index.php
+++ index.php
@@ -160,25 +160,25 @@
                         </div>
 
                 <div class="carrinho">
+                <table>  
+                    <tr>
+                        <td>Nome</td>
+                        <td>Qauntidade</td>
+                        <td>Preco</td>
+                    </tr>  
                 <?php
                     $query_product = "SELECT * FROM produto "; 
                     $result_product = $conexao->prepare($query_product);
                     $result_product->execute();   
                     
                      foreach ($result_product as $cliente ) : ?>
-                     <table>
                         <tr>
                             <td><?php echo $cliente['nome']; ?></td>
-                        </tr>
-                        <tr>
                             <td><?php echo $cliente['quantidade']; ?></td>
-                        </tr>
-                        <tr>
                             <td><?php echo $cliente['preco']; ?></td>
                         </tr>
-                     </table>   
                     <?php endforeach; ?>           
-
+                     </table>
                     <div class="novo-aplicativo">
                         <h3>Novo aplicativo</h3>
                         <h3>de E-commerce</h3>
@@ -190,240 +190,46 @@
                 
                 <div class="pedido-vendas">
                     <div class="pedido">
-                        <p>Pedidos</p>
-                        <p>No último mês</p>
-                        <button>Mais detalhes</button>
-
-                        <div>
-                            <img src="img/camisetaEle.png" alt="">
-                            <p>Elephant 1802</p>
-                            <p>To: Jason Bourne</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Pago</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <img src="img/camisetapreta.png" alt="">
-                            <p>RiseUP</p>
-                            <p>To: Marie Durant</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Processando</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <img src="img/camisetamarela.png" alt="">
-                            <p>Yellow Stone</p>
-                            <p>To: Dan Wilson</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Cancelado</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <img src="img/camisetaesporte.png" alt="">
-                            <p>Elephant 1802</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                            </div>
-                        </div>
+                        <table>    
+                            <tr>
+                                <td>Nome</td>
+                            </tr>
+                            <?php
+                                $query_product = "SELECT * FROM produto "; 
+                                $result_product = $conexao->prepare($query_product);
+                                $result_product->execute();   
+                                
+                                foreach ($result_product as $cliente ) : ?>
+                                    <tr>
+                                        <td><?php echo $cliente['nome']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>           
+                        </table>
                     </div>
 
                     <div class="venda">
-                        <table>
-                            <tr>
-                                <td>Item</td>
-                                <td>Produto</td>
-                                <td>Data</td>
-                                <td>Preço</td>
-                                <td>Status</td>
-                                <td>QTY</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <p>Macbook Air M1</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>Surface Laptop 4</p>
-                                    </div>
+                        <table>  
+                        <tr>
+                            <td>Nome</td>
+                            <td>data</td>
+                            <td>Preco</td>
+                            <td>Status</td>
+                            <td>Quantidade</td>
+                        </tr>  
+                            <?php
+                                $query_product = "SELECT * FROM produto "; 
+                                $result_product = $conexao->prepare($query_product);
+                                $result_product->execute();   
                                 
-                                    <div>
-                                        <p>Logitech MX 250</p>
-                                    </div>
-
-                                    <div>
-                                        <p>AudioEngine HD3</p>
-                                    </div>
-
-                                    <div>
-                                        <p>HP Hyper LTR</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Dell 32 UltraSharp</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Google Pixel 6 Pro</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>#XGY-356</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>#YHD-047</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>#SRR-678</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#PXF-578</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#PXF-778</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#XGY-356</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#XVR-425</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>02 Apr, 2023</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>01 Apr, 2023</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>24 Mar, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>24 Mar, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>16 Jan, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>22 Dec, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>27 Dec, 2023</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>R$100</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>In Stock</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>Out of Stock</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Out of Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Low Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>58 PCS</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>0 PCS</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>290 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>46 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>78 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>8 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>124 PCS</p>
-                                    </div>
-                                </td>
-                                
-                            </tr>     
+                                foreach ($result_product as $cliente ) : ?>
+                                    <tr>
+                                        <td><?php echo $cliente['nome']; ?></td>
+                                        <td><?php echo $cliente['data']; ?></td>
+                                        <td><?php echo $cliente['preco']; ?></td>
+                                        <td><?php echo $cliente['estado']; ?></td>
+                                        td><?php echo $cliente['quantidade']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>           
                         </table>
                     </div>
                 </div>
