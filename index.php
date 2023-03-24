5a3dd5aa890886e2b79dcf4585f2cda58791d26b
KamillySantana <kamillyferreira0300@gmail.com>
2023-03-24 16:11:21 -0300

    index

--- index.php
+++ index.php
@@ -77,8 +77,10 @@
                 <div class="painel-controle">
 
                     <div class="vendas-mes">
-                        <p>aqui vai ser o valor</p>
-                        <p>Vendas esse mês</p>
+                        <div>
+                            <p>aqui vai ser o valor</p>
+                            <p>Vendas esse mês</p>
+                        </div>
                         <i class="fa-solid fa-coins"></i>
                     </div>
 
@@ -105,333 +107,115 @@
                 <div class="carrinho">
                     <div class="carrinho-ativo">
                         <h4>Carrinhos ativos</h4>
-                        <button><img src="img/blusinha.PNG" alt=""></button>
-                        <p>Moda</p>
-                        <button><img src="img/game.PNG" alt=""></button>
-                        <p>Jogos</p>
-                        <button><img src="img/relogio.PNG" alt=""></button>
-                        <p>Eletrônicos</p>
-                        <button><img src="img/luvas.PNG" alt=""></button>
-                        <p>Esporte</p>
-                        <button><img src="img/tenis.PNG" alt=""></button>
-                        <p>Tênis</p>
 
-                        <table>
-                            <tr>
-                                <td>Item</td>
-                                <td>QTY</td>
-                                <td>Preço</td>
-                                <td>Valor total</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src="img/camisetaEle.png" alt="">
-                                        <p>Elephant 1802</p>
-                                        <p>Item: #XDG-2347</p>
-                                        
-                                    </div>
-                                    <div>
-                                        <img src="img/camisetarosa.png" alt="">
-                                        <p>Red Laga</p>
-                                        <p>Item:#XDG-1321</p>
-                                    </div>
-                                
-                                    <div>
-                                        <img src="img/camisetapreta.png" alt="">
-                                        <p>RiseUP</p>
-                                        <p>Item:#XDG-4312</p>
-                                        
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>X1</p>
-                                    </div>
-                                    <div>
-                                        <p>X2</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>X3</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>$72.00</p>
-                                    </div>
-                                    <div>
-                                        <p>$45.00</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>$84.00</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>$126.00</p>
-                                    </div>
-                                    <div>
-                                        <p>$76.00</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>$168.00</p>
-                                    </div>
-                                </td>
-                            </tr>     
+                        <div class="grid">
+                            <button>
+                                <div>
+                                    <img src="img/camiseta.png" alt="">
+                                </div>
+                                Moda
+                            </button>
+                            <button>
+                                <img src="img/jogos.png" alt="">
+                                Jogos
+                            </button>
+                            <button>
+                                <img src="img/relogio.PNG" alt="">
+                                Eletrônicos
+                            </button>
+                            <button>
+                                <img src="img/esporte.png" alt="">
+                                Esporte
+                            </button>
+                            <button>
+                                <img src="img/tenis.PNG" alt="">
+                                Tênis
+                            </button>
 
-                        </table>
+                            <div class="tabela-carrinho">
+                                <table border="1">
+                                    <th>ITEM</th>
+                                    <th>QTY</th>
+                                    <th>PREÇO</th>
+                                    <th>VALOR TOTAL</th>
+
+                                    <tr>
+                                        <td><img src="img/camisetaEle.png" alt="">
+                                            <p>Elephant 1802</p>
+                                            <p>Item: #XDG-2347</p>
+                                        </td>
+                                        <td>X1</td>
+                                        <td>$72.00</td>
+                                        <td>$126.00</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><img src="img/camisetarosa.png" alt="">
+                                            <p>Red Laga</p>
+                                            <p>Item: #XDG-1321</p>
+                                        </td>
+                                        <td>X2</td>
+                                        <td>$45.00</td>
+                                        <td>$76.00</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><img src="img/camisetapreta.png" alt="">
+                                            <p>RiseUP</p>
+                                            <p>Item: #XDG-4312</p>
+                                        </td>
+                                        <td>X3</td>
+                                        <td>$84.00</td>
+                                        <td>$168.00</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="novo-aplicativo">
-                        <h3>Novo aplicativo</h3>
-                        <h3>de E-commerce</h3>
-                        <img src="img/pessoa.PNG" alt="">
-                        <br>
-                        <button>Ver Novidades</button>
-                        <button>Ver Produtos</button>
+                    <div class="aplicativo">
+                        <h2>Novo aplicativo
+                        de  E-commerce</h2>
+
+                        <img src="img/pessoas.png" alt="">
+
+                        <div>
+                            <button>Ver Novidaes</button>
+                        </div>
+                        <div>
+                            <button>Ver Produtos</button>
+                        </div>
                     </div>
                 </div>
                 
-                <div class="pedido-vendas">
-                    <div class="pedido">
-                        <p>Pedidos</p>
-                        <p>No último mês</p>
-                        <button>Mais detalhes</button>
-
+                <div class="pedidos-vendas">
+                    <div class="pedidos">
                         <div>
-                            <img src="img/camisetaEle.png" alt="">
-                            <p>Elephant 1802</p>
-                            <p>To: Jason Bourne</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Pago</p>
-                            </div>
+                            <p>Pedidoa</p>
+                            <p>No último mês</p>
                         </div>
 
                         <div>
-                            <img src="img/camisetapreta.png" alt="">
-                            <p>RiseUP</p>
-                            <p>To: Marie Durant</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Processando</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <img src="img/camisetamarela.png" alt="">
-                            <p>Yellow Stone</p>
-                            <p>To: Dan Wilson</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                                <p>Cancelado</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <img src="img/camisetaesporte.png" alt="">
-                            <p>Elephant 1802</p>
-
-                            <div>
-                                <button><i class="fa-solid fa-ellipsis"></i></button>
-                            </div>
+                            <button>Mais detalhes</button>
                         </div>
                     </div>
 
-                    <div class="venda">
-                        <table>
-                            <tr>
-                                <td>Item</td>
-                                <td>Produto</td>
-                                <td>Data</td>
-                                <td>Preço</td>
-                                <td>Status</td>
-                                <td>QTY</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <p>Macbook Air M1</p>                                        
-                                    </div>
+                    <div class="vendas">
+                        <div>
+                            <p>Vendas</p>
+                            <p>Total de 3450 vendas recentes</p>
+                        </div>
 
-                                    <div>
-                                        <p>Surface Laptop 4</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>Logitech MX 250</p>
-                                    </div>
-
-                                    <div>
-                                        <p>AudioEngine HD3</p>
-                                    </div>
-
-                                    <div>
-                                        <p>HP Hyper LTR</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Dell 32 UltraSharp</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Google Pixel 6 Pro</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>#XGY-356</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>#YHD-047</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>#SRR-678</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#PXF-578</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#PXF-778</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#XGY-356</p>
-                                    </div>
-
-                                    <div>
-                                        <p>#XVR-425</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>02 Apr, 2023</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>01 Apr, 2023</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>24 Mar, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>24 Mar, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>16 Jan, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>22 Dec, 2023</p>
-                                    </div>
-
-                                    <div>
-                                        <p>27 Dec, 2023</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>R$100</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-
-                                    <div>
-                                        <p>R$100</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>In Stock</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>Out of Stock</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Out of Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>Low Stock</p>
-                                    </div>
-
-                                    <div>
-                                        <p>In Stock</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div>
-                                        <p>58 PCS</p>                                        
-                                    </div>
-
-                                    <div>
-                                        <p>0 PCS</p>
-                                    </div>
-                                
-                                    <div>
-                                        <p>290 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>46 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>78 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>8 PCS</p>
-                                    </div>
-
-                                    <div>
-                                        <p>124 PCS</p>
-                                    </div>
-                                </td>
-                                
-                            </tr>     
-                        </table>
+                        <div>
+                            <table>
+                                <th>Item</th>
+                                <th>PRODUTO</th>
+                                <th>DATA</th>
+                                <th>PREÇO</th>
+                                <th>STATUS</th>
+                                <th>Qty</th>
+                            </table>
+                        </div>
                     </div>
                 </div>
 
